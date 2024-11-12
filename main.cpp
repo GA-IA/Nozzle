7f4d74716fec7d117a86f7339dc209e5b514f263
GA_IA <flukefoxkung@gmail.com>
2024-11-12 12:56:52 +0700

    Link SDL3 to the project. Add seperate folder to Dependencies folder (SDL3 here).

--- main.cpp
+++ main.cpp
@@ -2,11 +2,39 @@
 //
 
 #include <iostream>
+#include <SDL3/SDL.h>
 
 using namespace std;
 
 int main()
 {
+	const int SCREEN_WIDTH = 1280;
+	const int SCREEN_HEIGHT = 720;
+	if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
+		cerr << SDL_GetError() << "\n";
+	}
+	SDL_Window* window = SDL_CreateWindow("Nozzle", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
+	if (!window) {
+		cerr << SDL_GetError() << "\n";
+	}
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+	if (!renderer) {
+		cerr << SDL_GetError() << "\n";
+	}
 	cout << "Hello Nozzle." << endl;
+	bool isRunning = true;
+	while (isRunning) {
+		SDL_Event sdlEvent;
+		SDL_PollEvent(&sdlEvent);
+
+		if (sdlEvent.type == SDL_EVENT_QUIT) {
+			isRunning = false;
+		}
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
