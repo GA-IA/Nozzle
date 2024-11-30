2ec16d4becacb1e2bd39f7ea42fc11d61e2eb1f1
GA_IA <flukefoxkung@gmail.com>
2024-12-01 01:17:57 +0700

    Add Imgui Node Editor Edit Editor.cpp and GUI.cpp

--- Sources/Editor.cpp
+++ Sources/Editor.cpp
@@ -10,7 +10,47 @@ void Editor::Event() {
 	
 }
 void Editor::Loop() {
-	ImGui::ShowDemoWindow();
+	ImGui::SetWindowFontScale(2);
+	ImGui::BeginMainMenuBar();
+	if (ImGui::BeginMenu("Files")) {
+		ImGui::MenuItem("New");
+		ImGui::MenuItem("Open");
+		if (ImGui::BeginMenu("Open recent")) {
+			ImGui::MenuItem("main.cpp");
+			ImGui::EndMenu();
+		}
+		ImGui::Separator();
+		ImGui::MenuItem("Save");
+		ImGui::MenuItem("Save as");
+		ImGui::EndMenu();
+	}
+	if (ImGui::BeginMenu("Edit")) {
+		ImGui::MenuItem("Undo");
+		ImGui::MenuItem("Redo");
+		ImGui::Separator();
+		ImGui::MenuItem("Copy");
+		ImGui::MenuItem("Paste");
+		ImGui::Separator();
+		ImGui::MenuItem("Preferences");
+		ImGui::EndMenu();
+	}
+	if (ImGui::BeginMenu("Selection")) {
+		ImGui::MenuItem("Select all");
+		ImGui::MenuItem("Duplicate current line");
+		ImGui::EndMenu();
+	}
+	if (ImGui::BeginMenu("Views")) {
+		if (ImGui::BeginMenu("Zoom")) {
+			ImGui::MenuItem("Zoom in");
+			ImGui::MenuItem("Zoom out");
+			ImGui::MenuItem("Restore zoom");
+			ImGui::EndMenu();
+		}
+		ImGui::EndMenu();
+	}
+	ImGui::EndMainMenuBar();
+
+	ImGui::DockSpaceOverViewport();
 }
 void Editor::Destroy() {
 	
--- Sources/GUI.cpp
+++ Sources/GUI.cpp
@@ -1,13 +1,17 @@
 #include "GUI.h"
 #include <iostream>
 
+using namespace std;
+
 int ImGuiSystem::Create(SDL_Window* window, SDL_Renderer* renderer) {
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	io = &ImGui::GetIO();
 	io->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-	io->ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+	//io->ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 	io->ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io->BackendFlags |= ImGuiBackendFlags_PlatformHasViewports;
+	io->BackendFlags |= ImGuiBackendFlags_RendererHasViewports;
 
 	ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
 	ImGui_ImplSDLRenderer3_Init(renderer);
@@ -26,6 +30,13 @@ void ImGuiSystem::Clear() {
 
 void ImGuiSystem::Present(SDL_Renderer* renderer) {
 	ImGui::Render();
+	if (io->ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+		// Update and Render additional Platform Windows
+		ImGui::UpdatePlatformWindows();
+		ImGui::RenderPlatformWindowsDefault();
+
+		// TODO for OpenGL: restore current GL context.
+	}
 	ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
 }
 
